16bb8449fc6d81a16970956257b856ff98ec3d94
Hrithik Rohilla <hrithikrohilla@gmail.com>
2020-09-21 22:59:34 +0530

    table random

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { importType } from '@angular/compiler/src/output/output_ast';
+import {RandomComponent} from './components/random.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,RandomComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,]
 })
 export class AppModule { }
---
+++ src/app/components/random.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'random',
+  templateUrl: './random.component.html'
+})
+export class RandomComponent {
+}
