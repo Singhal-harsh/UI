c5553e7b339571421f6386d5dcbace5d1c000f13
Harsh Singhal <singhal.harsh2@gmail.com>
2020-09-23 22:24:22 +0530

    Added divisions and improved bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,11 @@ import { AppComponent } from './app.component';
 import { importType } from '@angular/compiler/src/output/output_ast';
 import {RandomComponent} from './components/random.component';
 import { userTestComponent } from './components/userTest.component';
-import { Page2RandomComponent } from "./components/page2random.component";
 import {RouterModule, Router, Routes} from'@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent, RandomComponent, userTestComponent, Page2RandomComponent
+    AppComponent, RandomComponent, userTestComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/success.arb.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'success-arb',
-  templateUrl: './success.arb.component.html',
-})
-export class SuccessArbComponent {
-}
