e29dec0461b5c2c851dd8a42dbd8e2d8f445b54f
rucha <rucha.ranade@cumminscollege.in>
2020-09-24 09:11:20 +0530

    rucha commits print value

--- src/app/components/userTest.component.ts
+++ src/app/components/userTest.component.ts
@@ -9,12 +9,20 @@ import { formData } from './formData';
 export class userTestComponent{
 
     public currencies= ['USD','JPY','EUR','INR', 'GBP'];
+    public dataTrial = 0;
+
     
     formDataModel = new formData(105.38, 105.42, 105.75, 105.84, 4.95, 5.28, 0.31, 0.42);
 
     onSubmit(){
 
         console.log(this.formDataModel);
+    }
+
+    handleClickTrial(){
+
+        this.dataTrial = this.formDataModel.currencyOneAskRate;
+
     }
 
  }
