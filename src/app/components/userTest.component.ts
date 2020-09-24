51d0ca09ea03b2633c29bc3ed31621b41677b2a1
rucha <rucha.ranade@cumminscollege.in>
2020-09-24 08:31:52 +0530

    form data model created

--- src/app/components/userTest.component.ts
+++ src/app/components/userTest.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { formData } from './formData';
 
 @Component({
     selector : 'userTest',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
 })
 export class userTestComponent{
 
+    public currencies= ['USD','JPY','EUR','INR', 'GBP'];
+    
+    formDataModel = new formData(105.38, 105.42, 105.75, 105.84, 4.95, 5.28, 0.31, 0.42);
+
+    onSubmit(){
+
+        console.log(this.formDataModel);
+    }
 
  }
