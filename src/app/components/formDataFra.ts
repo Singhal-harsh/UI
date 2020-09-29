9bfd981dc9b66ce4ca4b50fab44c35dd80aa0dae
Hrithik Rohilla <hrithikrohilla@gmail.com>
2020-09-29 16:54:27 +0530

    service class built

---
+++ src/app/components/formDataFra.ts
@@ -0,0 +1,13 @@
+export class FormDataFra{
+
+	private interest_rate1_bid : number; //6 months
+	private  interest_rate1_ask:number; //6 months
+	private  interest_rate2_bid:number; //12 months
+	private  interest_rate2_ask:number; //12 months
+	
+	private  fra_rate_bid:number; /* 6/12 fra */
+	private  fra_rate_ask:number; /* 6/12 fra */
+	
+	private  transaction_cost:number;
+	private  notional_amount:number;
+}
---
+++ src/app/services/user.service.fra.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import {Observable} from 'rxjs';
+
+import {formData} from '../components/formData';
+import {outputData} from '../components/outputData';
+
+@Injectable({
+    providedIn: 'root',
+  })
+export class UserService {
+
+    public baseurl : string = "http://localhost:8084";
+
+    public httpClient :  HttpClient;
+    constructor (httpClient : HttpClient)
+    {
+        this.httpClient = httpClient;
+    }
+
+    // addProduct(productObj : any){
+    //     console.log('Product pushed into an array: ');
+    // }
+
+    sendInput(inputobj : any): Observable<Object> {
+        console.log('sendInput called');
+        return this.httpClient.post(this.baseurl+'/userFRAArbitrage', inputobj,{headers: 
+            {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' } });
+    }
+
+    getRandom(): Observable<any> {
+        console.log("Get Random called");
+        return this.httpClient.get(`${this.baseurl}` + '/randomFRAArbitrage', {headers: 
+            {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }});
+    }
+
+    // updateProduct(id:number,value:any): Observable<Object> {
+    //     console.log('!!!!!' + id);
+    //     console.log(this.baseurl+'/product/'+id);
+    //     return this.httpClient.put(this.baseurl+'/product/' + id,value);
+
+    // }
+
+    // deleteProduct(id:number): Observable<any> {
+    //     console.log('$$$' + id);
+    //     console.log(this.baseurl+'/product/'+id);
+    //     return this.httpClient.delete(this.baseurl+'/product/' + id, {responseType:'text'});
+    //     window.history.go(0);
+    // }
+}
